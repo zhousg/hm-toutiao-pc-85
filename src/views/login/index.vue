9added332b18d4adcd6cdd14d0d955e5582f56fc
zhousg <zhoushugang@itcast.cn>
2019-10-25 18:26:56 +0800

    11-登录模块-绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,19 +1,44 @@
 <template>
   <!-- 全屏容器 -->
-  <div class='container'>
+  <div class="container">
     <!-- 卡片容器  el-card是一个element组件，根元素上默认添加一个类和组件的名称一致 -->
     <el-card>
-      <img src="../../assets/logo_index.png" alt="">
+      <img src="../../assets/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form :model="LoginForm">
+        <el-form-item>
+          <el-input v-model="LoginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="LoginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
-.container{
+.container {
   width: 100%;
   height: 100%;
   background: pink;
@@ -24,17 +49,17 @@ export default {}
   // 组合写法：background:..... / 背景图尺寸(width,height)
   // 特殊写法：cover  等比例缩放铺满容器多余不显示  contain 等比例缩放完全显示在容器内
   background: url(../../assets/login_bg.jpg) no-repeat center / cover;
-  .el-card{
+  .el-card {
     width: 400px;
     height: 350px;
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
-    img{
+    transform: translate(-50%, -50%);
+    img {
       width: 200px;
       display: block;
-      margin: 0 auto;
+      margin: 0 auto 30px;
     }
   }
 }
