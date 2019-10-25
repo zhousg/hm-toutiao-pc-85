0f7215b118076db81f84e462adf3ec1e48f50cbb
zhousg <zhoushugang@itcast.cn>
2019-10-25 18:06:38 +0800

    10-登录模块-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class='container'>Login</div>
+  <!-- 全屏容器 -->
+  <div class='container'>
+    <!-- 卡片容器  el-card是一个element组件，根元素上默认添加一个类和组件的名称一致 -->
+    <el-card>
+      <img src="../../assets/logo_index.png" alt="">
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container{
+  width: 100%;
+  height: 100%;
+  background: pink;
+  position: absolute;
+  left: 0;
+  top: 0;
+  // 背景图尺寸：拆分写法 background-size
+  // 组合写法：background:..... / 背景图尺寸(width,height)
+  // 特殊写法：cover  等比例缩放铺满容器多余不显示  contain 等比例缩放完全显示在容器内
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  .el-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    img{
+      width: 200px;
+      display: block;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
