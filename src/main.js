42b70dd5ec39c6636d4aeed02f8da5914a32249e
zhousg <zhoushugang@itcast.cn>
2019-10-25 16:08:59 +0800

    项目初始化完毕

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main.js的作用是入口文件
+// 职责1：创建一个根实例
+// 职责2：复杂全局范围的依赖导入
